d2ae10cddcc6a5aebfd076d8b82b0e8105ac2b32
Opeiz <ja.opazoy@gmail.com>
2024-02-21 01:49:12 +0100

    Final presentation 1

--- plot.py
+++ plot.py
@@ -7,11 +7,28 @@ file_path = 'results.txt'
 data = pd.read_csv(file_path)
 print(data)
 
+subset = data[data['Epochs'] == 50].copy()
+
+booleans = subset['MixUp'].unique()
+
 x_column = 'Epochs'
 y_column = 'Accuracy'
 z_column = 'Learning Rate'
+color_tab = 'MixUp'
+params_col = 'Parameters'
 
-plt.scatter(data[x_column], data[y_column], cmap='viridis')
-plt.xlabel(x_column)
+conditions = [
+    subset[color_tab] == booleans[0],
+    subset[color_tab] == booleans[1]
+]
+
+choices = ['green', 'blue']
+
+colors = np.select(conditions, choices, default='black')
+plt.scatter(subset[params_col], subset[y_column], c=colors, alpha=0.5)
+plt.xlabel(params_col)
 plt.ylabel(y_column)
-plt.show()
+plt.title(f'{y_column} vs {params_col}')
+plt.legend(booleans)
+# plt.show()
+# plt.savefig('images/Tr
