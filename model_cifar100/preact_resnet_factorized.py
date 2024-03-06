4f9b5a0841a91fd226af7722eca98fa21f209e23
Emmanuela TERTULIANO MOREIRA DE SOUSA <e22tertu@sl-tp-br-510.imta.fr>
2024-03-06 17:03:11 +0100

    testing factorization

--- model_cifar100/preact_resnet_factorized.py
+++ model_cifar100/preact_resnet_factorized.py
@@ -6,19 +6,39 @@ Reference:
 '''
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as F 
+from torchsummary import summary
+import numpy as np
 
 
 class PreActBlock(nn.Module):
     '''Pre-activation version of the BasicBlock.'''
     expansion = 1
-
+    @classmethod
+    def set_factorization_level(cls,fact_level = 1):
+        cls.fact_level = fact_level
     def __init__(self, in_planes, planes, stride=1):
         super(PreActBlock, self).__init__()
         self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        if self.fact_level == 0:
+            self.bn1 = nn.BatchNorm2d(in_planes)
+            self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            self.bn2 = nn.BatchNorm2d(planes)
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        if self.fact_level == 1:
+            self.conv_dw = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False)
+            self.bn_dw = nn.BatchNorm2d(in_planes)
+            self.conv_pw = nn.Conv2d(in_planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
+            self.bn_pw = nn.BatchNorm2d(planes)
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        if self.fact_level == 2:
+            self.conv_dw = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False)
+            self.bn_dw = nn.BatchNorm2d(in_planes)
+            self.conv_pw = nn.Conv2d(in_planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
+            self.bn_pw = nn.BatchNorm2d(planes)
+            self.conv2_dw = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, groups=planes, bias=False)
+            self.bn2_dw = nn.BatchNorm2d(planes)
+            self.conv2_pw = nn.Conv2d(planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
 
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
@@ -28,8 +48,23 @@ class PreActBlock(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(x))
         shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
-        out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
+        if self.fact_level == 0:
+            out = self.conv1(out)
+            out = self.conv2(F.relu(self.bn2(out)))
+        if self.fact_level == 1:
+            out = self.conv_dw(out)
+            out = self.bn_dw(out)
+            out = self.conv_pw(out)
+            out = self.conv2(F.relu(self.bn_pw(out)))
+        if self.fact_level == 2:
+            out = self.conv_dw(out)
+            out = self.bn_dw(out)
+            #out = self.conv_pw(out)
+            #out = self.conv2_dw(F.relu(self.bn_pw(out)))
+            out = F.relu(self.bn_pw(self.conv_pw(out)))
+            out = self.conv2_dw(out)
+            out = self.bn2_dw(out)
+            out = self.conv2_pw(out)
         out += shortcut
         return out
 
@@ -94,7 +129,8 @@ class PreActResNet(nn.Module):
         return out
 
 
-def PreActResNet18A(): #modifing the name to avoid confusion with the other file factorized
+def PreActResNet18(fact_level):
+    PreActBlock.set_factorization_level(fact_level=fact_level)
     return PreActResNet(PreActBlock, [1,1,1,1])
 
 def PreActResNet34():
@@ -110,9 +146,9 @@ def PreActResNet152():
     return PreActResNet(PreActBottleneck, [3,8,36,3])
 
 
-def test():
-    net = PreActResNet18()
+def test(fact_level):
+    net = PreActResNet18(fact_level)
     y = net((torch.randn(1,3,32,32)))
     print(y.size())
 
-# test()
+
